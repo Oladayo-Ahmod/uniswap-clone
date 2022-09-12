7c6594a03116d0aa10296ee068872ca4c6bb1dc3
oladayo ahmod <oladayoahmod112@gmail.com>
2022-09-12 18:19:27 +0100

    main component design

--- front-end/components/Main.jsx
+++ front-end/components/Main.jsx
@@ -15,6 +15,15 @@ const Main =()=>{
                 background-color:#191B1F;
                 height:300px;
             }
+            input{
+                background :#20242A;
+                border-color : #20242A;
+                height : 80px;
+            }
+            .input-group span{
+                background-color :#2D242F;
+                border-color :#2D242F;
+            }
             `
             }
 
@@ -24,7 +33,23 @@ const Main =()=>{
                     <div className="col-md-5 mt-5 rounded rounded-bg px-2">
                        <div className="col-md-12 pt-2 d-flex justify-content-between">
                             <p className='text-white font-bold'>Swap</p>
-                            <p className='text-white font-bold'>2</p>
+                            <div className="dropdown">
+                            <button className="btn shadow btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Eth
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            </div>
+                       </div>
+                       <div className="col-md-12 pt-2 d-flex justify-content-between">
+                           <div className="input-group">
+                            <span className='input-group-text text-white'>0.0</span>
+                             <input type="text"
+                               className="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+                           </div>
                        </div>
                     </div>  
                 </div>
