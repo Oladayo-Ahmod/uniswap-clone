f1ba549b059bc3083515847bca7720f379e2b50b
oladayo ahmod <oladayoahmod112@gmail.com>
2022-11-10 14:41:41 +0100

    patch users with transactions in sanity

--- front-end/context/TransactionContext.js
+++ front-end/context/TransactionContext.js
@@ -5,9 +5,18 @@ import Swal from 'sweetalert2'
 import {client} from '../lib/Sanity'
 
 
-// useEffect(()=>{
-//     const Swal = require()
-// },[])
+useEffect(()=>{
+   if(!currentAccount) return ;
+    (async ()=>{
+        const userDoc = {
+            _type : 'users',
+            _id : currentAccount,
+            userName : 'a user',
+            address : currentAccount
+        }
+        await client.createIfNotExists(userDoc)
+    })()
+},[currentAccount])
 
 const TransactionContext = React.createContext()
 const contractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
